1ec1154c026cae6435d4b7b0e2822801d9a3ff0c
obaldia <deobaldia@hsu-hh.de>
2017-06-20 14:47:55 +0200

    Sample rate now accepted by Generator

--- wavenet/utils.py
+++ wavenet/utils.py
@@ -10,7 +10,10 @@ def normalize(data):
 
 
 def make_batch(path):
-    data = wavfile.read(path)[1][:, 0]
+	# A "batch" is made up by the whole samples
+
+    # data = wavfile.read(path)[1][:, 0]
+    [rate, data] = wavfile.read(path)
 
     data_ = normalize(data)
     # data_f = np.sign(data_) * (np.log(1 + 255*np.abs(data_)) / np.log(1 + 255))
@@ -22,4 +25,4 @@ def make_batch(path):
 
     # Encode targets as ints.
     targets = (np.digitize(data_[1::], bins, right=False) - 1)[None, :]
-    return inputs, targets
+    return inputs, targets, rate
